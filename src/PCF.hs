94eab3f7f02f5f0688153545ddabc9a6105d9ced
joey <joeyvelez@knights.ucf.edu>
2021-01-18 18:46:29 -0500

    reformatted some comments

--- src/PCF.hs
+++ src/PCF.hs
@@ -24,42 +24,42 @@ import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Reader
 
-{- ========================================= Syntax ================================================ -}
+{- ================================= Syntax ================================= -}
 
 type Id = String
 
-data Term = TmUnit               -- Unit               {Introduction forms}
-          | TmTrue               -- True
-          | TmFalse              -- False
-          | TmVar Id             -- Variables
-          | TmProd Term Term     -- Products
-          | TmFun Id Type Term   -- Functions
-          | TmLet Term Term      -- Let statements     {Elimination forms}
-          | TmIf Term Term Term  -- If statements
-          | TmFst Term           -- First projection
-          | TmSnd Term           -- Second projection
-          | TmApp Term Term      -- Application
+data Term = TmUnit                -- Unit               {Introduction forms}
+          | TmTrue                -- True
+          | TmFalse               -- False
+          | TmVar  Id             -- Variables
+          | TmProd Term Term      -- Products
+          | TmFun  Id Type Term   -- Functions
+          | TmLet  Term Term      -- Let statements     {Elimination forms}
+          | TmIf   Term Term Term -- If statements
+          | TmFst  Term           -- First projection
+          | TmSnd  Term           -- Second projection
+          | TmApp  Term Term      -- Application
           deriving (Show, Eq)
 
-data Type = TyUnit               -- Unit
-          | TyBool               -- Booleans
-          | TyProd Type Type     -- Products
-          | TyFun Type Type      -- Functions
+data Type = TyUnit                -- Unit
+          | TyBool                -- Booleans
+          | TyProd Type Type      -- Products
+          | TyFun  Type Type      -- Functions
           deriving (Show, Eq)
 
 type Binding = (Id, Type)         -- e.g. x :: Bool => (x,Bool)
 type Context = [Binding]
 
 
-{- ========================================= Semantics ========================================== -}
+{- ================================= Syntax ================================= -}
 
-data Error = EVar Id
-           | ELet Term
-           | EIf1 Term
-           | EIf2 Term Term
-           | EProd Term
-           | EFun1 Term Term
-           | EFun2 Term
+data Error = EVar  Id         -- Variable not in context
+           | ELet  Term       -- First term isn't Unit type
+           | EIf1  Term       -- First term isn't Bool type
+           | EIf2  Term Term  -- Second and third term aren't the same type
+           | EProd Term       -- Term isn't product type
+           | EFun1 Term Term  -- Second term not valid iput to first term
+           | EFun2 Term       -- First term isn't a funtion
            deriving (Show)
 
 -- Typecheck type = Reader + Either monad stack
@@ -72,8 +72,8 @@ tyCheck :: Term -> TcType
 tyCheck (TmUnit)           = return TyUnit
 tyCheck (TmTrue)           = return TyBool
 tyCheck (TmFalse)          = return TyBool
-tyCheck (TmVar x)          = do ctx <- ask                 -- get the current context
-                                ty  <- find x              -- find variable's type in the context
+tyCheck (TmVar x)          = do ctx <- ask
+                                ty  <- find x
                                 return ty
 tyCheck (TmProd tm1 tm2)   = do ty1 <- tyCheck tm1
                                 ty2 <- tyCheck tm2
@@ -82,41 +82,42 @@ tyCheck (TmFun x ty1 tm)   = do ty2 <- tyCheck tm
                                 return $ TyFun ty1 ty2
 tyCheck (TmLet tm1 tm2)    = do ty1 <- tyCheck tm1
                                 lift $ if ty1 == TyUnit
-                                         then Left $ ELet tm1      -- tm1's type must be Unit
+                                         then Left $ ELet tm1
                                          else Right ()
                                 ty2 <- tyCheck tm2
                                 return ty2
 tyCheck (TmIf tm1 tm2 tm3) = do ty1 <- tyCheck tm1
                                 lift $ if ty1 == TyBool
-                                         then Left $ EIf1 tm1      -- tm1's type must be Bool
+                                         then Left $ EIf1 tm1
                                          else Right ()
                                 ty2 <- tyCheck tm2
                                 ty3 <- tyCheck tm3
                                 lift $ if ty2 == ty3
-                                         then Left $ EIf2 tm2 tm3  -- tm2 and tm3's type must match
+                                         then Left $ EIf2 tm2 tm3
                                          else Right ()
                                 return ty3
 tyCheck (TmFst tm)         = do ty <- tyCheck tm
                                 lift $ case ty of
                                          (TyProd _ _) -> Right ()
-                                         _            -> Left $ EProd tm -- tm must be a product
+                                         _            -> Left $ EProd tm
                                 return ty
 tyCheck (TmSnd tm)         = do ty <- tyCheck tm
                                 lift $ case ty of
                                          (TyProd _ _) -> Right ()
-                                         _            -> Left $ EProd tm -- tm must be a product
+                                         _            -> Left $ EProd tm
                                 return ty
 tyCheck (TmApp tm1 tm2)    = do ty1 <- tyCheck tm1
                                 ty2 <- tyCheck tm2
                                 lift $ case ty1 of
-                                         (TyFun ty11 ty12) -> if ty11 == ty2
-                                                                then Right ty12
-                                                                else Left $ EFun1 tm1 tm2 -- tm2 not valid input to tm1
-                                         _                 -> Left $ EFun2 tm1            -- tm1 must be a function
-
+                                         (TyFun ty11 ty12)
+                                           | ty11 == ty2 -> Right ty12
+                                           | otherwise   -> Left $ EFun1 tm1 tm2
+                                         _                 -> Left $ EFun2 tm1
 
 find :: Id -> TcType
 find x = do ctx <- ask
             lift $ case lookup x ctx of
                      Nothing -> Left $ EVar x
                      Just ty -> Right ty
+
+
